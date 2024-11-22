80036440fcbba19782f0db19e43cbf69607e0948
XC-Xinze <xchen7@gradcenter.cuny.edu>
2024-11-21 22:01:15 -0500

    upload

---
+++ MainFrame.py
@@ -0,0 +1,18 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QSplitter, QTreeView, QFileSystemModel, QTextBrowser
+from PySide6.QtCore import Qt
+#Create main window 
+#Inherite from QMainWindow as the children class
+class  CardMakerMain(QMainWindow):
+    def __init__(self):
+        super().__init__()#Construct this class
+        self.setWindowTitle('CardMaker')#title
+        self.resize(400, 300) #size
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)# need QApplication for starting using any GUI
+                              # sys.argv is for transfer parameters of booting into program
+    window=CardMakerMain()
+    window.show()  # 显示窗口
+    sys.exit(app.exec())  # 
+
